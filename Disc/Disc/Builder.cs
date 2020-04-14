f326aa7bead3573f7c61633ce8e50da976485d78
Dyachenko K <kostyan.tsk@gmail.com>
2020-04-14 17:57:40 +0700

    Выполнил построение основного диска.

--- Disc/Disc/Builder.cs
+++ Disc/Disc/Builder.cs
@@ -16,39 +16,119 @@ namespace Disc
         /// </summary>
         /// <param name="iPart"> интерфейс детали</param>
         /// <param name="kompas">  объект типа kompas</param>
-        /// <param name="hiveparams"> класс, хранящий параметры улья</param>
+        /// <param name="discparams"> класс, хранящий параметры улья</param>
         public void Build(ksPart iPart, KompasObject kompas, DiscParams discparams)
         {
             this.iPart = iPart;
             CreateDisc(iPart, kompas, discparams);
         }
+
         /// <summary>
-        /// Функция выполняет построение основной части улья.
+        /// Функция выполняет построение основного диска.
         /// </summary>
         public void CreateDisc(ksPart iPart, KompasObject kompas, DiscParams discParams)
         {
-            //double thickness = discParams.DiscParams;
+            //Толщина диска
+            int thickness = discParams.Width;
+            //Радиус диска
+            int radius = discParams.MainDiameter / 2;
             ksEntity iSketch;
 
             ksSketchDefinition iDefinitionSketch;
 
-           //CreateSketch(out iSketch, out iDefinitionSketch);
+            CreateSketch(out iSketch, out iDefinitionSketch);
+
             // Интерфейс для рисования = на скетче;
-            //ksDocument2D iDocument2D = (ksDocument2D)iDefinitionSketch.BeginEdit();
-            // Построить прямоугольник (x1,y1, x2,y2, style)
-            ksRectangleParam par1 = (ksRectangleParam)kompas.GetParamStruct((short)StructType2DEnum.ko_RectangleParam);
-            par1.ang = 0; //Угол ?
-            par1.x = 10;
-            par1.y = 10;
-            //par1.width = discParams.HiveLength;
-            //par1.height = discParams.HiveHeight; // Больше похоже на ширину, нежели высоту.
-            par1.style = 1; // При нуле не видно деталь.
-            //iDocument2D.ksRectangle(par1);
+            ksDocument2D iDocument2D = (ksDocument2D)iDefinitionSketch.BeginEdit();
+
+            iDocument2D.ksCircle(0, 0, radius, 1);
 
             // Закончить редактировать эскиз
-            //iDefinitionSketch.EndEdit();
+            iDefinitionSketch.EndEdit();
 
-           // ExctrusionSketch(iPart, iSketch, thickness, true);
+            ExctrusionSketch(iPart, iSketch, thickness, true);
+        }
+
+        /// <summary>
+        /// Создать эскиз
+        /// </summary>
+        /// <param name="iSketch">Эскиз</param>
+        /// <param name="iDefinitionSketch">Определение эскиза</param>
+        /// <param name="offset">Смещение от начальной плоскости</param>
+        private void CreateSketch(out ksEntity iSketch, out ksSketchDefinition iDefinitionSketch, double offset = 0)
+        {
+            #region Создание смещенную плоскость -------------------------
+            // интерфейс смещенной плоскости
+            ksEntity iPlane = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_planeOffset);
+
+            // Получаем интрефейс настроек смещенной плоскости
+            ksPlaneOffsetDefinition iPlaneDefinition = (ksPlaneOffsetDefinition)iPlane.GetDefinition();
+
+            // Настройки : начальная позиция, направление смещения, расстояние от плоскости, принять все настройки (create)
+            iPlaneDefinition.SetPlane(iPart.GetDefaultEntity((short)Obj3dType.o3d_planeXOZ));
+            iPlaneDefinition.direction = true;
+            iPlaneDefinition.offset = offset;
+            iPlane.Create();
+            #endregion --------------------------------------------------
+
+            // Создаем обьект эскиза
+            iSketch = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_sketch);
+
+            // Получаем интерфейс настроек эскиза
+            iDefinitionSketch = iSketch.GetDefinition();
+
+            // Устанавливаем плоскость эскиза
+            iDefinitionSketch.SetPlane(iPlane);
+
+            // Теперь когда св-ва эскиза установлены можно его создать 
+            iSketch.Create();
+        }
+
+        /// <summary>
+        /// Выдавливание по эскизу
+        /// </summary>
+        /// <param name="iPart">Интерфейс детали</param>
+        /// <param name="iSketch">Эскиз</param>
+        /// <param name="depth">Глубина выдавливания</param>
+        /// <param name="direction">Направление выдавливания</param>
+        private void ExctrusionSketch(ksPart iPart, ksEntity iSketch, double depth, bool direction)
+        {
+            //Операция выдавливание
+            ksEntity entityExtr = (ksEntity)iPart.NewEntity((short)Obj3dType.o3d_bossExtrusion);
+
+            //Интерфейс операции выдавливания
+            ksBossExtrusionDefinition extrusionDef = (ksBossExtrusionDefinition)entityExtr.GetDefinition();
+
+            //Интерфейс структуры параметров выдавливания
+            ksExtrusionParam extrProp = (ksExtrusionParam)extrusionDef.ExtrusionParam();
+
+            //Эскиз операции выдавливания
+            extrusionDef.SetSketch(iSketch);
+
+            //Направление выдавливания
+            if (direction == false)
+            {
+                extrProp.direction = (short)Direction_Type.dtReverse;
+            }
+            else
+            {
+                extrProp.direction = (short)Direction_Type.dtNormal;
+            }
+
+            //Тип выдавливания
+            extrProp.typeNormal = (short)End_Type.etBlind;
+
+            //Глубина выдавливания
+            if (direction == false)
+            {
+                extrProp.depthReverse = depth;
+            }
+            else
+            {
+                extrProp.depthNormal = depth;
+            }
+            //Создание операции выдавливания
+            entityExtr.Create();
         }
     }
 }
